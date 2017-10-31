91f5aa5a4286c7e84cf669b796d76a9b2591f82f
Xiaotao Wang <wangxiaotao868@163.com>
2017-10-31 19:32:19 +0800

    robust installation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# TADLib documentation build configuration file, created by
-# sphinx-quickstart on Tue May  2 09:18:37 2017.
+# runHiC documentation build configuration file, created by
+# sphinx-quickstart on Mon Oct 30 10:04:10 2017.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -30,7 +30,11 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.githubpages','numpydoc']
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.coverage',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -88,7 +92,6 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-
 html_sidebars = {
     '**': [
         'about.html',
@@ -151,7 +154,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'runHiC', u'runHiC Documentation',
+    (master_doc, 'runhic', u'runHiC Documentation',
      [author], 1)
 ]
 
@@ -171,4 +174,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {}
+intersphinx_mapping = {'https://docs.python.org/': None}
