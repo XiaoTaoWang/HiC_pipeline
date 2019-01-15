12b63d50fb32553d6daded2f11a83c43af6d13d9
Xiaotao Wang <wangxiaotao686@gmail.com>
2019-01-15 14:04:30 -0500

    large dataset overflow

--- runHiC/binning.py
+++ runHiC/binning.py
@@ -15,14 +15,14 @@ def binning_from_pairs(pairpath, res, outpath, ignore_diags=1, nproc=1, mad_max=
     log.log(21, 'Building contact matrix ...')
     bin_label = ':'.join([chromsizes_file, str(res)])
     bin_command = ['cooler', 'cload', 'pairix', '--assembly', assembly, '--nproc', str(nproc),
-                   bin_label, pairpath, outpath]
+                   '--max-split', str(max_split), bin_label, pairpath, outpath]
     subprocess.check_call(' '.join(bin_command), shell=True)
     log.log(21, 'Done')
 
     log.log(21, 'Perform ICE ...')
     ice_command = ['cooler', 'balance', '--nproc', str(nproc), '--ignore-diags', str(ignore_diags),
                    '--mad-max', str(mad_max), '--min-nnz', str(min_nnz), '--min-count', str(min_count),
-                   '--max-split', str(max_split), outpath]
+                   outpath]
     subprocess.check_call(' '.join(ice_command), shell=True)
     log.log(21, 'Done')
 
