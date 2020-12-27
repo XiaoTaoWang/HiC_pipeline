ed077a0dcccb4452c4f27aa5495fa529e942eb26
Xiaotao Wang <wangxiaotao686@gmail.com>
2020-12-27 15:19:22 -0600

    additional logging messages

--- runHiC/binning.py
+++ runHiC/binning.py
@@ -20,7 +20,7 @@ def mcool_from_pairs(pairpath, outcool, outmcool, ignore_diags=2, nproc=1, mad_m
     subprocess.check_call(' '.join(bin_command), shell=True)
     log.log(21, 'Done')
 
-    log.log(21, 'Generate a multi-resolution cooler file by coarsening the 1kk contact matrix ...')
+    log.log(21, 'Generate a multi-resolution cooler file by coarsening the 1kb contact matrix ...')
     command = ['cooler', 'zoomify', '-p', str(nproc), '-r 1000,2000,5000,10000,25000,50000,100000,250000,500000,1000000,2500000,5000000',
                '--balance', '--balance-args "--mad-max" {0} "--min-nnz" {1} "--min-count" {2} "--ignore-diags" {3}'.format(mad_max, min_nnz, min_count, ignore_diags),
                '-o', outmcool, outcool]
